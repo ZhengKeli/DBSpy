22134eba08cfecf93a2fc99d63f812e3a3c8e0da
ZhengKeli <zhengkeli2009@126.com>
2019-04-14 20:20:54 +0700

    fix a bug in indexing

--- py/spector/utils/indexing.py
+++ py/spector/utils/indexing.py
@@ -2,19 +2,19 @@ def search_nearest(x_head, x_tail, x_step, y, func):
     x_last = x_head
     y_last = func(x_last)
     asc = y_last < y
-    x = x_last + x_step
-    while x < x_tail:
-        yx = func(x)
-        if (yx < y) != asc:
-            if (yx - y < y - y_last) == asc:
-                return x, y
+    x_this = x_last + x_step
+    while x_this < x_tail:
+        y_this = func(x_this)
+        if (y_this < y) != asc:
+            if (y_this - y < y - y_last) == asc:
+                return x_this, y_this
             else:
                 return x_last, y_last
-        x_last = x
-        y_last = y
-        x = x_last + x_step
+        x_last = x_this
+        y_last = y_this
+        x_this += x_step
     return x_last, y_last
 
 
-def index_nearest(x, xs):
-    return search_nearest(0, len(xs), 1, x, lambda i: xs[i])[0]
+def index_nearest(value, array):
+    return search_nearest(0, len(array), 1, value, lambda i: array[i])[0]
