9caf128251e9accfa65043648b21917c27f3759e
ZhengKeli <zhengkeli2009@126.com>
2019-06-06 11:23:39 +0700

    simplified

--- core/py/dbspy/core/spectrum/dbs/peak/_peak.py
+++ core/py/dbspy/core/spectrum/dbs/peak/_peak.py
@@ -3,17 +3,15 @@ from scipy import ndimage
 
 from dbspy.core import base
 from dbspy.utils.indexing import index_nearest
+from dbspy.utils.neighborhood import neighborhood
 from dbspy.utils.variance import spectrum_var
 
 
 # define
 
 class Conf(base.ElementConf):
-    def __init__(self, search_center=None, search_radius=None, search_range=None, peak_center=None, peak_radius=None):
-        self.search_center = search_center
-        self.search_radius = search_radius
+    def __init__(self, search_range=None, peak_radius=None):
         self.search_range = search_range
-        self.peak_center = peak_center
         self.peak_radius = peak_radius
 
 
@@ -25,32 +23,14 @@ class Process(base.ElementProcess):
 def process_func(raw_result, conf: Conf):
     x, y = raw_result
     y_var = spectrum_var(y)
+    search_range_i = (0, len(x)) if conf.search_range is None \
+        else index_nearest(conf.search_range, x)
     
-    if conf.search_range is not None:
-        search_range = conf.search_range
-    elif conf.search_center is not None and conf.search_radius is not None:
-        search_range = (conf.search_center - conf.search_radius, conf.search_center + conf.search_radius)
-    else:
-        search_range = None
+    y_blur = ndimage.gaussian_filter1d(y, 3.0)
+    peak_center_i = search_peak_center_i(y_blur, *search_range_i)
+    peak_center = x[peak_center_i]
     
-    if conf.peak_radius is not None:
-        peak_radius = conf.peak_radius
-    elif conf.search_radius is not None:
-        peak_radius = conf.search_radius
-    else:
-        raise RuntimeError("Cannot define the radius of peak.")
-    
-    if conf.peak_center is not None:
-        peak_center = conf.peak_center
-        peak_center_i = index_nearest(peak_center, x)
-    elif search_range is not None:
-        y_blur = ndimage.gaussian_filter1d(y, 3.0)
-        peak_center_i = search_peak_center_i(y_blur, *index_nearest(search_range, x))
-        peak_center = x[peak_center_i]
-    else:
-        raise RuntimeError("Cannot define the index of peak center.")
-    
-    peak_range = (peak_center - peak_radius, peak_center + peak_radius)
+    peak_range = neighborhood(peak_center, conf.peak_radius)
     peak_range_i = index_nearest(peak_range, x)
     
     peak_x = x[slice(*peak_range_i)]
