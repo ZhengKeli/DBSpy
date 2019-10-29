dd36b988bfcf77f72fa8b70bcee04d4f2083fb1c
ZhengKeli <zhengkeli2009@126.com>
2019-10-29 23:21:19 +0300

    simplified the project structure

---
+++ src/dbspy/gui/spectrum/_spectrum.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+
+import matplotlib.pyplot as plt
+
+from dbspy import FigureController
+from dbspy import base
+
+
+class Controller(base.ProcessController):
+    def __init__(self, app, index):
+        self.app = app
+        self.index = index
+        self.result_controller = None
+        self.conf_tag = tk.Variable()
+        super().__init__(app.container, app.process.spectrum_processes[index])
+    
+    def on_create_info_frame(self, info_frame):
+        tk.Label(info_frame, text=f'#{self.index} DBS Spectrum').pack()
+        
+        tag_frame = tk.Frame(info_frame)
+        tk.Label(tag_frame, text='tag=').pack(side='left')
+        tk.Entry(tag_frame, width=6, textvariable=self.conf_tag).pack(side='left')
+        tk.Button(tag_frame, text='OK', command=self.apply_tag).pack(side='left')
+        tag_frame.pack()
+        
+        tk.Button(info_frame, text='Remove', foreground='red', command=self.remove).pack()
+    
+    def on_create_result_frame(self, result_frame):
+        self.result_controller = FigureController(result_frame, plt.figure(), self.on_draw_result)
+        self.result_controller.widget.pack(anchor='w', fill='both')
+    
+    def on_update(self, result, exception):
+        self.conf_tag.set(str(self.process.tag))
+        self.result_controller.draw(result, exception)
+    
+    @staticmethod
+    def on_draw_result(figure, result, _):
+        axe = figure.gca()
+        if result is not None:
+            (x, y, _), _ = result
+            axe.semilogy(x, y)
+        else:
+            axe.set_title("Error!")
+            # todo show info
+    
+    def apply_tag(self):
+        tag = self.conf_tag.get()
+        try:
+            tag = int(tag)
+        except ValueError:
+            try:
+                tag = float(tag)
+            except ValueError:
+                pass
+        self.process.tag = tag
+        self.app.update_tree()
+    
+    def remove(self):
+        self.app.process.remove_spectrum_process(self.process)
+        self.app.update_frame(['main'])
+        self.app.update_tree()
---
+++ src/dbspy/gui/spectrum/dbs/bg/_bg.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+from collections import Iterable
+
+import matplotlib.pyplot as plt
+
+from dbspy import FigureResultController
+from dbspy import base
+from dbspy import dbs
+from dbspy.core.spectrum.dbs import Conf
+
+
+class Controller(FigureResultController, base.ElementProcessController):
+    def __init__(self, app, index):
+        self.spectrum_process: dbs.Process = app.process.spectrum_processes[index]
+        self.index = index
+        self.conf_radius = tk.StringVar()
+        self.conf_left_expand = tk.StringVar()
+        self.conf_right_expand = tk.StringVar()
+        super().__init__(
+            app.container,
+            self.spectrum_process.bg_process,
+            plt.figure(figsize=(6, 3)))
+    
+    def on_create_info_frame(self, info_frame):
+        tk.Label(info_frame, text='Background').pack()
+    
+    def on_create_conf_frame(self, conf_frame):
+        tk.Label(conf_frame, text='radius=').pack(side='left')
+        tk.Entry(conf_frame, width=6, textvariable=self.conf_radius).pack(side='left')
+        tk.Label(conf_frame, text='eV').pack(side='left')
+        
+        tk.Label(conf_frame, text='left_expand=').pack(side='left', padx=(10, 0))
+        tk.Entry(conf_frame, width=6, textvariable=self.conf_left_expand).pack(side='left')
+        tk.Label(conf_frame, text='eV').pack(side='left')
+        
+        tk.Label(conf_frame, text='right_expand=').pack(side='left', padx=(10, 0))
+        tk.Entry(conf_frame, width=6, textvariable=self.conf_right_expand).pack(side='left')
+        tk.Label(conf_frame, text='eV').pack(side='left')
+    
+    def on_reset(self, conf: Conf):
+        self.conf_radius.set(str(conf.bg_radius))
+        
+        if isinstance(conf.bg_expand, Iterable):
+            left_expand, right_expand = conf.bg_expand
+        else:
+            left_expand = right_expand = conf.bg_expand
+        self.conf_left_expand.set(str(left_expand))
+        self.conf_right_expand.set(str(right_expand))
+    
+    def on_apply(self) -> Conf:
+        left_expand = float(self.conf_left_expand.get())
+        right_expand = float(self.conf_right_expand.get())
+        return Conf(
+            bg_radius=float(self.conf_radius.get()),
+            bg_expand=(left_expand, right_expand))
+    
+    def on_update_draw(self, figure, result, exception):
+        axe = figure.gca()
+        if result is not None:
+            _, (peak_x, peak_y, _), peak_center_i = self.spectrum_process.peak_process.value
+            bg_range_i, (bg_x, bg_y) = result
+            
+            plt.semilogy(
+                (peak_x[peak_center_i], peak_x[peak_center_i]),
+                (0, 1.3 * peak_y[peak_center_i]), '--', color='black')
+            axe.semilogy(peak_x, peak_y, label='raw')
+            axe.semilogy(bg_x, bg_y, label='background')
+        else:
+            axe.set_title("Error!")
+            # todo show info
